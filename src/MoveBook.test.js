72862f16b286dcca41986fe6e6148ab08c5b4f16
Augusto Fabian Rodrigues <augustofabianrodrigues@gmail.com>
2020-09-23 22:40:36 -0300

    test: Add MoveBook tests

--- src/MoveBook.test.js
+++ src/MoveBook.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import MoveBook from './MoveBook';
 
@@ -41,4 +42,59 @@ test('does not call onMove with the current shelf', () => {
 
   currentlyReadingOption.simulate('click');
   expect(handleMove).not.toHaveBeenCalled();
+});
+
+test('<MoveBook /> should show currentlyReading option as selected', () => {
+  const component = renderer.create(
+    <MemoryRouter>
+      <MoveBook shelf="currentlyReading" moving={false} onMove={jest.fn()} />
+    </MemoryRouter>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('<MoveBook /> should show wantToRead option as selected', () => {
+  const component = renderer.create(
+    <MemoryRouter>
+      <MoveBook shelf="wantToRead" moving={false} onMove={jest.fn()} />
+    </MemoryRouter>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('<MoveBook /> should show read option as selected', () => {
+  const component = renderer.create(
+    <MemoryRouter>
+      <MoveBook shelf="read" moving={false} onMove={jest.fn()} />
+    </MemoryRouter>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('<MoveBook /> should show none option as selected', () => {
+  const component = renderer.create(
+    <MemoryRouter>
+      <MoveBook shelf="none" moving={false} onMove={jest.fn()} />
+    </MemoryRouter>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('<MoveBook /> should show the moving indicator', () => {
+  const component = renderer.create(
+    <MemoryRouter>
+      <MoveBook shelf="currentlyReading" moving={true} onMove={jest.fn()} />
+    </MemoryRouter>
+  );
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
